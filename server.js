86d7ba188cc97daf8ab4d6fbcba23a34695d989f
ogus02 <ogus0247@gmail.com>
2021-09-30 10:19:15 +0200

    server set up

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 8042;
+
+app.get('/', (req, res) => {
+
+    fs.readFile('index.html', (err, data) => {
+        res.setHeader('Content-Type', 'text/html');
+        res.send(data);
+  });
+
+});
+
+app.listen(PORT, () => {
+console.log(`Example app listening at http://localhost:${PORT}`);
+});
